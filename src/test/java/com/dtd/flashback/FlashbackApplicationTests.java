ac9aacf1c31e5cb634cb4192070f121698026cfc
dtdWi <dtd.010110@gmail.com>
2021-12-10 00:52:43 +0100

    inicjalizing

---
+++ src/test/java/com/dtd/flashback/FlashbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtd.flashback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
